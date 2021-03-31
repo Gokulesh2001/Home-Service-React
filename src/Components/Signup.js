8b36d1fc35181514e3470863b52244b940207485
Sabarirajtg <tg.sabariraj@gmail.com>
2021-03-31 19:40:05 +0530

    basic sign up done

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,44 +1,31 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import React, { useState, useEffect } from "react";
+import firebaseDb from "../Firebase";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -47,6 +34,43 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignUp() {
+  const InititalValues = {
+    fname: "",
+    lname: "",
+    Email: "",
+    Password: "",
+  };
+
+  var [values, setValues] = useState(InititalValues);
+
+  const handleInputChange = (e) => {
+    var { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    setValues({ ...InititalValues });
+    // firebaseDb.database().ref("Login/").push("hello");
+  }, []);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("fn called");
+    firebaseDb
+      .database()
+      .ref("Login/")
+      .child(values.Email)
+      .set({
+        FirstName: values.fname,
+        LastName: values.lname,
+        Email: values.Email,
+        Password: values.Password,
+      });
+  };
+
   const classes = useStyles();
 
   return (
@@ -64,12 +88,13 @@ export default function SignUp() {
             <Grid item xs={12} sm={6}>
               <TextField
                 autoComplete="fname"
-                name="firstName"
+                name="fname"
                 variant="outlined"
+                label="first Name"
                 required
                 fullWidth
-                id="firstName"
-                label="First Name"
+                value={values.fname}
+                onChange={handleInputChange}
                 autoFocus
               />
             </Grid>
@@ -80,8 +105,10 @@ export default function SignUp() {
                 fullWidth
                 id="lastName"
                 label="Last Name"
-                name="lastName"
+                name="lname"
                 autoComplete="lname"
+                value={values.lname}
+                onChange={handleInputChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -91,8 +118,10 @@ export default function SignUp() {
                 fullWidth
                 id="email"
                 label="Email Address"
-                name="email"
+                name="Email"
                 autoComplete="email"
+                value={values.Email}
+                onChange={handleInputChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -100,29 +129,34 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
-                name="password"
+                name="Password"
                 label="Password"
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={values.Password}
+                onChange={handleInputChange}
               />
             </Grid>
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
                 label="I want to receive inspiration, marketing promotions and updates via email."
               />
-            </Grid>
+            </Grid> */}
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleFormSubmit}
+            >
+              Sign up
+            </Button>
+          </Grid>
           <Grid container justify="flex-end">
             <Grid item>
               <Link href="#" variant="body2">
@@ -132,9 +166,6 @@ export default function SignUp() {
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
